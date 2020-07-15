0a0a71844e7afc1fab6601deb6bbb28082eb3c9c
Kimyeongho <dudgh9661@naver.com>
2020-07-15 16:08:09 +0900

    study and practice about query and resolves

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,9 @@
+import { people, getById } from "./db";
+
 const resolvers = {
     Query : {
-        name : () => "yeongho"
+        people : () => people,
+        person : (_, {id}) => getById(id)
     }
 }
 
